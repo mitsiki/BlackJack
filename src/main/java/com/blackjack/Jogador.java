680d3160729c0b6c61f6f2e58ccb19089d64da63
Matheus Soares <mths.soares@outlook.com>
2025-02-01 22:36:06 -0300

    Update Jogador.java

--- src/main/java/com/blackjack/Jogador.java
+++ src/main/java/com/blackjack/Jogador.java
@@ -7,22 +7,28 @@ import java.util.ArrayList;
  * Classe que simula a mão de cartas de um jogador durante um jogo de cartas.
  */
 public class Jogador {
+    // Array que armazena a mão de cartas do jogador.
     private ArrayList<Carta> mao;
+
+    // Soma dos valores das cartas na mão do jogador.
     private int pontuacao;
 
+    // Constrói a mão e pontuação iniciais do jogador.
     public Jogador() {
         mao = new ArrayList<>();
         pontuacao = 0;
     }
 
+    // Adiciona uma carta na mão do jogador.
     public void receberCarta(Carta carta) {
         mao.add(carta);
-        pontuacao += carta.getPontuacao();
+        pontuacao += carta.getValor();
+        
         // Ajusta o valor do Ás se a pontuação ultrapassar 21
         if (pontuacao > 21) {
             for (Carta c : mao) {
-                if (c.getValor().equals("Ás") && c.getPontuacao() == 11) {
-                    c = new Carta(c.getNaipe(), c.getValor(), 1); // Ás agora vale 1
+                if (c.getNaipe().equals("Ás") && c.getValor() == 11) {
+                    c = new Carta(c.getNaipe(), c.getSimbolo(), 1); // Ás agora vale 1
                     pontuacao -= 10; // Ajusta a pontuação
                 }
             }
